a4e51dc251ae0cc9c55d96e4c62d5efec14c46d1
Peter Martic <98329726+sxcxk@users.noreply.github.com>
2023-11-22 17:20:51 +0100

    Add files via upload
    Added LightHubService

---
+++ LightHubService_MARTIC/LightHubService.Core/Color.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightHubService.Core
+{
+    public enum Color
+    {
+        Brown = 0,
+        DarkBlue = 1,
+        DarkGreen = 2,
+        SeaBlue = 3,
+        DarkRed = 4,
+        Violet = 5,
+        GrassGreen = 6,
+        Grey = 7,
+        DarkGrey = 8,
+        Blue = 9,
+        Green = 10,
+        Cyan = 11,
+        Red = 12,
+        Magenta = 13,
+        Yellow = 14,
+        White = 15
+    }
+}
---
+++ LightHubService_MARTIC/LightHubService.Core/UdpService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightHubService.Core
+{
+    public class UdpService
+    {
+        private Socket _udpSocket;
+        private IPEndPoint _ipendpoint;
+        private MessageQueue _messageQueue;
+        private IServiceLogger _serviceLogger;
+        public UdpService(int port, IServiceLogger serviceLogger)
+        {
+            if (port < 1025 | port > 65536)
+            {
+                port = 1512;
+            }
+            _serviceLogger = serviceLogger;
+            _ipendpoint = new IPEndPoint(IPAddress.Loopback, port);
+            _udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+
+
+        public void Start()
+        {
+            _udpSocket.Bind(_ipendpoint);
+            _serviceLogger.LogSysInformation(DateTime.Now.ToString() + "- " + _udpSocket.LocalEndPoint.ToString() + "-Aktion: " + "gestartet");
+            _messageQueue = new MessageQueue(_udpSocket, _serviceLogger);
+            ThreadStart _threadStart = new ThreadStart(_messageQueue.HandleIncomingMessages);
+            Thread _thread = new Thread(_threadStart);
+            _thread.IsBackground = true;
+            _thread.Start();
+
+
+        }
+
+        public void Stop()
+        {
+            _serviceLogger.LogSysInformation(DateTime.Now.ToString() + "- " + _udpSocket.LocalEndPoint.ToString() + "-Aktion: " + "gestoppt");
+            _udpSocket.Shutdown(SocketShutdown.Both);
+            _udpSocket.Close();
+        }
+    }
+}
